f8177e88b8cf43f9a8b2fa3120b99364058930c3
Pratham Bhandary <prathamb1501@gmail.com>
2026-05-25 19:39:57 +0530

    changed app.add to .route

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.get("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
