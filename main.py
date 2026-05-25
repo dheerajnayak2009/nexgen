f4ca3ae511bb6681e92bfa16ad302fb027309bb2
Dheeraj <ddnayak103@gmail.com>
2026-05-25 18:15:10 +0530

    Updated this stuff

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.get("/")
 def home():
     return render_template("index.html")
+
+@app.get("/login")
+def login():
+    return render_template("login.html")
+
+# --- IF LOCAL, THEN UNCOMMENT THIS ---
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
