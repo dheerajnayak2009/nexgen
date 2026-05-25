0c29ae16230b130109d2bd051337cb4e5f2993b7
Pratham Bhandary <prathamb1501@gmail.com>
2026-05-25 19:52:19 +0530

    added the entire login logic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
+
+URL = "https://sharmaji.pythonanywhere.com"
 
 @app.route("/")
 def home():
@@ -8,6 +11,25 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        response = requests.post(f"{URL}/login",
+                    json = {
+                        "username" : username,
+                        "password" : password
+                    })
+        
+        if response.status_code == 200:  # Successful login
+            return f"Login Successful for {response.json().get('role')}"
+        
+        if response.status_code == 401:  # Invalid credentials
+            return "Invalid username or password. Please try again."
+        
+        if response.status_code == 400: # Missing fields(empty data sent to sharmaji)
+            return "Please fill in all the fields."
+        
     return render_template("login.html")
 
 # --- IF LOCAL, THEN UNCOMMENT THIS ---
