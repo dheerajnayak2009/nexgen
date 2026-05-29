33e222e0ac45cebe6c6d00930e9a0fa2757a0011
Dheeraj <ddnayak103@gmail.com>
2026-05-29 16:59:05 +0530

    fingerprinting and staff profile done

--- main.py
+++ main.py
@@ -532,5 +532,5 @@ def staff_profile(username):
 # =========================
 # RUN LOCALLY
 # =========================
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
